f0b7376b82f1a7bc1bf7de324fa4ea4ad4b24c08
wmsi <wchurch@whitemountainscience.org>
2016-08-12 10:45:17 -0400

    Create helloworld.js

---
+++ helloworld.js
@@ -0,0 +1,26 @@
+(function(ext) {
+    // Cleanup function when the extension is unloaded
+    ext._shutdown = function() {};
+
+    // Status reporting code
+    // Use this to report missing hardware, plugin or unsupported browser
+    ext._getStatus = function() {
+        return {status: 2, msg: 'Ready'};
+    };
+  
+    // Function called for Hello World block
+    ext.hello_world = function() {
+      alert("Hello, world!");
+    }
+    
+    // Block and block menu descriptions
+    var descriptor = {
+        blocks: [
+          // Block type, block name, function name
+          [" ", "Hello world", "hello_world"],
+        ]
+    };
+
+    // Register the extension
+    ScratchExtensions.register('Hello World', descriptor, ext);
+})({});
