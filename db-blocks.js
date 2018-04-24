9263d7269762f5c262bdc3c5df42d166c51be263
Marc Bucchieri <wchurch@whitemountainscience.org>
2018-04-24 09:16:40 -0700

    started work on db-blocks extension for data stories project

--- db-blocks.js
+++ db-blocks.js
@@ -3,7 +3,8 @@
     ext._shutdown = function() {};
 
     var formdata;
-    var request = new XMLHttpRequest();
+    var request;
+    var DEFAULT_POST = 'https://wmsinh.org/scratchx';
 
     // Status reporting code
     // Use this to report missing hardware, plugin or unsupported browser
@@ -12,7 +13,10 @@
     };
 
     ext.append_formdata = function(element_name, element_value) {
-        // Code that gets executed when the block is run
+        if(!form_data) {
+            alert('You must open a request before you can add elements');
+            return;
+        }
         formdata.append(element_name, element_value);
         console.log('Element ' + element_name + ' has been added with the value ' + element_value);
     };
@@ -27,13 +31,31 @@
     }
     
     ext.send_request = function() {
+        if(!request) {
+            alert('You must open a request before you can send it');
+            return;
+        }
         request.send(formdata);
         console.log('Sent FormData: ' + formdata);
+        formdata = null;
+        request = null;
     }
     
     ext.add_request_header = function(header_name, header_value) {
+        if(!request) {
+            alert('You must open a request before you can add headers to it');
+            return;
+        }
         request.setRequestHeader(header_name, header_value)
         console.log('Added request header with a name of ' + header_name + ' and a value of ' + header_value)
+    }
+
+    ext.post_data = function(project_id, data_type, value) {
+        open_request('POST', DEFAULT_POST);
+        append_formdata('project_id', project_id);
+        append_formdata('data_type', data_type);
+        append_formdata('value', value);
+        send_request();
     }
 
     // Block and block menu descriptions
@@ -41,9 +63,10 @@
         blocks: [
             // Block type, block name, function name
             [' ', 'add element %s %s', 'append_formdata', 'element name', 'element value'],
-            [' ', 'open request %m.method %s', 'open_request', 'POST', 'https://wmsinh.org/data-story'],
+            [' ', 'open request %m.method %s', 'open_request', 'POST', DEFAULT_POST],
             [' ', 'send request', 'send_request'],
-            [' ', 'add request header (beta) %s %s', 'add_request_header', 'header name', 'header value']
+            [' ', 'post data to project %n with data_type %s and value %n', 'post_data', '0', 'tempC', '25']
+            // [' ', 'add request header (beta) %s %s', 'add_request_header', 'header name', 'header value']
         ],
         menus:{
             method: ['POST', 'GET']
