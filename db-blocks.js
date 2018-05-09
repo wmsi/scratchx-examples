cc371c0723b28839a8c71f33dfa70df8d07ae165
Marc Bucchieri <wchurch@whitemountainscience.org>
2018-05-09 13:48:05 -0700

    changed default data type to loudness

--- db-blocks.js
+++ db-blocks.js
@@ -131,7 +131,7 @@
             // [' ', 'open request %m.method %s', 'open_request', 'POST', DEFAULT_URL],
             // [' ', 'send request', 'send_request'],
             // [' ', 'post data to project %n with sensor id %n and data_type %s and value %n', 'post_data', '0', '0', 'tempC', '25'],
-            [' ', 'post data to project %n with data_type %s and value %n', 'post_data', '0', 'tempC', '25'],
+            [' ', 'post data to project %n with data_type %s and value %n', 'post_data', '0', 'loudness', '25'],
             [' ', 'pull data from project %n with data_type %s', 'pull_data', '0', 'tempC'],
             ['r', 'data set length', 'get_data_length'],
             ['r', 'data set string', 'get_data_string'],
